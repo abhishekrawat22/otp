33e72cc66edda0660a4e06b44b9c94ef224cd4a6
abhishekrawat22 <abhishekrawat5743@gmail.com>
2023-03-26 12:33:44 +0530

    Updated validation for number of wrong attempts.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ otpInput.forEach((el, index) => {
         let value = el.value;
     
         // Remove any non-numeric characters
-        value = value.replace(/[^0-9]/g, "1");
+        value = value.replace(/[^0-9]/g, "");
     
         // Limit the input to a single digit
         if (value.length > 1) {
@@ -27,11 +27,11 @@ otpInput.forEach((el, index) => {
     el.addEventListener('keyup', () => {
         // To shift focus to the next input when the single digit is entered to the current one.
         if(el.value.length >= 1) {
-            otpInput[index + 1].focus();
+            otpInput[index + 1]?.focus();
         }
         // To go to the previous input when current's value is cleared.
         else if(el.value == '') {
-            otpInput[index - 1].select();
+            otpInput[index - 1]?.focus();
         }
     })
 })
@@ -40,6 +40,8 @@ otpInput.forEach((el, index) => {
 // To get each input value into the one string and then check with the correct OTP.
 const form = document.getElementsByTagName('form')[0];
 let fullOtp = [];
+let correctOtp = "123456";
+let attempts = 0;
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // We call e.preventDefault to prevent the default submit behavior so we can do client-side form submission.
     const formData = new FormData(form); // Then we create the formData object with the FormData constructor with the form as the argument to get the form data values.
@@ -47,12 +49,19 @@ form.addEventListener('submit', (e) => {
         fullOtp.push(pair[1]);
     }
     let a = fullOtp.join(''); // To get entered OTP as a single string.
-    if (a == "123456") {
+    if (a == correctOtp) {
         document.getElementsByTagName('body')[0].innerText = "Entered OTP is correct.";
     }
     else {
-        alert("Entered OTP is incorrect. Please retry.");
-        form.reset();
-        otpInput[0].focus();
+        attempts++;
+        if (attempts < 3) {
+            alert(`Entered OTP is incorrect. You have ${3 - attempts} attempts left. Please retry.`);
+            form.reset();
+            otpInput[0].focus();
+            fullOtp = [];
+        } else {
+            alert("You have exceeded the maximum number of attempts. Please try again later.");
+            window.location.reload();
+        }
     }
 })

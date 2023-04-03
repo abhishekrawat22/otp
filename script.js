2eef606ef7cd334b6e5d45bb025ca25a85b8366a
abhishekrawat22 <abhishekrawat5743@gmail.com>
2023-04-03 23:30:09 +0530

    Implemented functionality to copy OTP from the clipboard.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 let otpInput = document.querySelectorAll("[name=otp]"); // Get all the inputs.
 let verifyOtpBtn = document.getElementsByTagName('button')[0]; // Get verify button.
+const pasteBtn = document.querySelector('#paste-btn'); // Get paste button.
+
+// To keep button disabled till all the fields are filled.
+const checkInputs = () => {
+    let validInputs = Array.from(otpInput).filter( input => input.value !== "");
+    if(validInputs.length == 6) {
+        verifyOtpBtn.classList.remove("disabled");
+    }
+}
+
+// To paste OTP from the clipboard on the click of a button.
+const pasteOTP = () => {
+    let clipboardData = navigator.clipboard.readText(); // Will read content from the clipboard
+    clipboardData.then((clipboardText) => {
+        const digits = clipboardText.match(/\d+/g); // Checking if the content is in the number format.
+        const firstValue = Object.values(digits)[0].split(''); // Getting the recently copied number as a string and splitting it into single digit and storing in an array.
+        if (firstValue && firstValue.length > 0) {
+            otpInput.forEach((el, index) => {
+                if (index <= firstValue.length) {
+                    el.value = firstValue[index];
+                    otpInput[index + 1]?.focus();
+                } else {
+                    el.value = '';
+                }
+            })
+            checkInputs();
+        }
+    })
+}
+pasteBtn.addEventListener('click', () => {
+    pasteOTP();
+});
 
 otpInput.forEach((el, index) => {
     // Listen for the "input" event on the input element
@@ -18,7 +50,7 @@ otpInput.forEach((el, index) => {
     el.addEventListener('keyup', (event) => {
         // To shift focus to the next input when the single digit is entered to the current one.
         const key = event.key;
-        if(el.value.length >= 1) {
+        if(el.value.length >= 1 && (key >= 96 || key <= 105)) {
             otpInput[index + 1]?.focus();
         }
         // To go to the previous input when current's value is cleared.
@@ -26,13 +58,27 @@ otpInput.forEach((el, index) => {
             verifyOtpBtn.classList.add("disabled");
             otpInput[index - 1]?.focus();
         }
-        // To keep button disabled till all the fields are filled.
-        let validInputs = Array.from(otpInput).filter( input => input.value !== "");
-        if(validInputs.length == 6) {
-            verifyOtpBtn.classList.remove("disabled");
-        }
+        checkInputs();
     })
 })
+
+// To pastae OTP using keyboard keys.
+otpInput[0].addEventListener("keydown", function (ev) {
+
+    // function to check the detection
+    ev = ev || window.event;  // Event object 'ev'
+    var key = ev.which || ev.keyCode; // Detecting keyCode
+    
+    // Detecting Ctrl
+    var ctrl = ev.ctrlKey ? ev.ctrlKey : ((key === 17)
+        ? true : false);
+    
+    // If key pressed is V and if ctrl is true.
+    if (key == 86 && ctrl) {
+        pasteOTP();
+    }
+    
+});
 
 // To get each input value into the one string and then check with the correct OTP.
 const form = document.getElementsByTagName('form')[0];
